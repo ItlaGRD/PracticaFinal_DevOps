9a8bdb77e7ee427c63f1d4890f1bf615d6ac2b99
Gerny <20153109@itla.edu.do>
2025-04-17 11:38:05 -0400

    Corregi el test

--- index.test.js
+++ index.test.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
+
 test('Check for description in HTML', () => {
     const html = fs.readFileSync('./index.html', 'utf8');
-    expect(html).toMatch(/Gerny Feliz Matrícula: 2015-3109/);
+    expect(html).toMatch(/<i><b>Gerny Feliz<\/b><\/i><br>Matrícula: 2015-3109/);
 });
 
+test('Check for repository button', () => {
+    const html = fs.readFileSync('./index.html', 'utf8');
+    expect(html).toMatch(/https:\/\/github\.com\/ItlaGRD\/PracticaFinal_DevOps\.git/);
+});
